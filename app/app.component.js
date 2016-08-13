b142f4d9d90e2cab72befdef7cfabce644bf20cb
MetalMor <srgark@gmail.com>
2016-08-13 12:39:22 +0200

    130816: template separada del componente app.component.html: + añadida la template del componente app.component.ts: * template sustituida por el path al fichero html que la contiene

--- app/app.component.js
+++ app/app.component.js
@@ -21,7 +21,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>{{ title }}</h1>'
+            templateUrl: 'app/app.component.html'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
